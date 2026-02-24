5bf9fa0007755d60025b8cd40783aadf05cd8bf3
divinoe <divinoe@host.docker.internal>
2026-02-24 15:30:41 +0100

    implementato ivaDto

--- src/main/java/com/xantrix/webapp/services/IvaService.java
+++ src/main/java/com/xantrix/webapp/services/IvaService.java
@@ -1,25 +1,46 @@
 package com.xantrix.webapp.services;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.xantrix.webapp.dtos.IvaDto;
 import com.xantrix.webapp.entities.Iva;
 import com.xantrix.webapp.repository.IvaRepository;
 
 @Service
 public class IvaService {
-	
+
 	@Autowired
 	private IvaRepository repository;
-	
-	
-	public List<Iva> ivaListaService(){
-		return repository.getAll();
+
+	@Autowired
+	private ModelMapper modelMapper;
+
+	public List<IvaDto> ivaListaService() {
+		List<Iva> listIvaEntity = repository.getAll();
+		List<IvaDto> listIvaDto = new ArrayList<IvaDto>();
+
+		List<IvaDto> mapping = listIvaEntity.stream().map(element -> modelMapper.map(element, IvaDto.class))
+				.collect(Collectors.toList());
+		listIvaDto.addAll(mapping);
+
+		return listIvaDto;
 	}
-	
-	public Iva ivaDetailsService(String id) {
-		return repository.getById(id);
+
+	public IvaDto ivaDetailsService(String id) {
+		Iva iva = repository.getById(id);
+
+		if (iva != null) {
+			IvaDto ivaDto = modelMapper.map(iva, IvaDto.class);
+
+			return ivaDto;
+		}
+
+		return null;
 	}
 }
